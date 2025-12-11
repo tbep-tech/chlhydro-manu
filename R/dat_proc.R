7f7eb1b4a5c5160b5407d6def74ad8d04dcd3485
fawda123 <mbafs2012@gmail.com>
2025-12-11 16:43:03 -0500

    working mods done

--- R/dat_proc.R
+++ R/dat_proc.R
@@ -10,19 +10,19 @@ source(here('R/funcs.R'))
 
 # water quality
 wqdat <- epcdata |> 
-  filter(yr > 1974 & yr <= 2024) |> 
+  filter(yr > 1975 & yr <= 2024) |> 
   select(bay_segment, SampleTime, tn, chla, Sal_Top_ppth, Sal_Mid_ppth, Sal_Bottom_ppth) |>
   mutate(
-    sal = mean(c(Sal_Top_ppth, Sal_Mid_ppth, Sal_Bottom_ppth), na.rm = T), 
+    sal = Sal_Top_ppth, #mean(c(Sal_Top_ppth, Sal_Mid_ppth, Sal_Bottom_ppth), na.rm = T), 
     .by = c(bay_segment, SampleTime)
   ) |> 
   mutate(
     date = floor_date(as.Date(SampleTime), unit = 'month')
   ) |> 
   summarise(
-    tn = median(tn, na.rm = T), 
-    chla = median(chla, na.rm = T), 
-    sal = median(sal, na.rm = T),
+    tn = mean(tn, na.rm = T), 
+    chla = mean(chla, na.rm = T), 
+    sal = mean(sal, na.rm = T),
     .by = c(bay_segment, date)
   ) |> 
   mutate(
@@ -31,36 +31,44 @@ wqdat <- epcdata |>
     chla = ifelse(chla == 0, NA, chla),
     bay_segment = factor(bay_segment, levels = c('OTB', 'HB', 'MTB', 'LTB'))
   ) |> 
-  arrange(bay_segment, date)
+  arrange(bay_segment, date) |> 
+  mutate(
+    salorig = sal,
+    sal = zoo::na.approx(salorig, x = date, na.rm = F, maxgap = 3), 
+    .by = c(bay_segment)
+  )
+
+# salinity NA by bay segment
+wqdat |> 
+  summarise(
+    n_sal_na = sum(is.na(salorig)),
+    n_tot = n(),
+    .by = c(bay_segment)
+  ) |> 
+  mutate(
+    perc_na = n_sal_na / n_tot * 100
+  )
 
 # OTB model --------------------------------------------------------------
 
 tomod <- wqdat |> 
   filter(bay_segment == 'HB')
 
-mod <- gam(chla ~ te(dec_time, doy, sal, bs = c('tp', 'cc', 'tp')), data = tomod, 
-knots = list(doy=c(1,366)), family = Gamma(link = 'log'))
-
-mod <- gam(chla ~ s(dec_time, k = 150, bs = 'tp') + 
-  s(doy, bs = 'cc') +                       
-  s(sal, bs = 'tp') +
-  ti(dec_time, doy, bs = c('tp', 'cc')) +
-  ti(dec_time, sal, bs = c('tp', 'tp')) +
-  ti(sal, doy, bs = c('tp', 'cc')),
+mod <- gam(chla ~ s(dec_time, k = 40, bs = 'tp') + 
+  s(doy, k = 10, bs = 'cc') + 
+  s(sal, k = 10) +
+  ti(dec_time, doy, k = c(5, 5), bs = c('tp', 'cc')) +
+  ti(dec_time, sal, k = c(5, 5)) +
+  ti(sal, doy, k = c(5, 5), bs = c('tp', 'cc')),
   data = tomod,
-  knots = list(doy=c(1,366)),
-  family = Gamma(link = 'log'), 
-)
-
-mod <- gam(chla ~ s(dec_time, k = 300, bs = 'tp') +                   
-  s(sal, k = 50, bs = 'tp') +
-  ti(dec_time, sal, bs = c('tp', 'tp')),
-  data = tomod,
-  family = Gamma(link = 'log') 
+  family = Gamma(link = 'log'),
+  knots = list(doy=c(0,366)),
+  method = 'REML'
 )
 
 summary(mod)$dev.expl
-
+gam.check(mod)
+acf(residuals(mod, type = "deviance")) # check for autocorrelation in residds
 prds <- pred_fun(tomod, mod)
 
 ggplot(prds, aes(x = dec_time, y = chla)) +
@@ -73,7 +81,6 @@ grid_plo(prds, month = 'all', allsal = T, years = c(2000, 2024))
 grid_plo(prds)
 grid_plo(prds, month = c(5:10), years = c(2000, 2024), allsal = F, ncol = 6, sal_fac = 6)
 
-
 toplo <- data.frame(prds) |> 
   mutate(
     yr = lubridate::year(date)
@@ -83,7 +90,6 @@ toplo <- data.frame(prds) |>
     btnorm = mean(btnorm, na.rm = T),
     .by = c(yr)
   )
-
 
 ggplot(toplo, aes(x = yr, y = btfit)) +
   geom_point() + 
@@ -101,18 +107,19 @@ ggplot(toplo, aes(x = date, y = chla)) +
 # all bay segments -------------------------------------------------------
 
 mods <- wqdat |> 
-  # filter(dec_time >= 2000) |
+  # filter(dec_time >= 2000) |>
   group_nest(bay_segment) |> 
   mutate(
-    mod = map(data, ~ gam(chla ~ s(dec_time, k = 150, bs = 'tp') + 
-      s(doy, k = 20, bs = 'cc') +                       
-      s(sal, bs = 'tp') +
-      ti(dec_time, doy, bs = c('tp', 'cc')) +
-      ti(dec_time, sal, bs = c('tp', 'tp')) +
-      ti(sal, doy, bs = c('tp', 'cc')),
-      data = .x,
-      knots = list(doy=c(1,366)),
-      family = Gamma(link = 'log'), 
+    mod = map(data, ~ gam(chla ~ s(dec_time, k = 40, bs = 'tp') + 
+          s(doy, k = 10, bs = 'cc') + 
+          s(sal, k = 10) +
+          ti(dec_time, doy, k = c(5, 5), bs = c('tp', 'cc')) +
+          ti(dec_time, sal, k = c(5, 5)) +
+          ti(sal, doy, k = c(5, 5), bs = c('tp', 'cc')),
+        data = .x,
+        knots = list(doy=c(0,366)),
+        family = Gamma(link = 'log'),
+        method = 'REML'
       )
     ),
     prds = map2(data, mod, pred_fun), 
@@ -130,10 +137,65 @@ mods <- wqdat |>
     )
   )
 
+save(mods, file = here('data/mods.RData'))
+
+load(file = here('data/mods.RData'))
+
+# run gam.check, edf should be < k', high p-values might suggest auto-correlation
+gam.check(mods$mod[[1]])
+gam.check(mods$mod[[2]])
+gam.check(mods$mod[[3]])
+gam.check(mods$mod[[4]])
+
+# check autocor at lag 1, should be < 0.2
+acf(residuals(mods$mod[[1]], type = "deviance"), plot = F, lag.max = 1)$acf[2]
+acf(residuals(mods$mod[[2]], type = "deviance"), plot = F, lag.max = 1)$acf[2]
+acf(residuals(mods$mod[[3]], type = "deviance"), plot = F, lag.max = 1)$acf[2]
+acf(residuals(mods$mod[[4]], type = "deviance"), plot = F, lag.max = 1)$acf[2]
+
+summary(mods$mod[[1]])$dev.expl
+summary(mods$mod[[2]])$dev.expl
+summary(mods$mod[[3]])$dev.expl
+summary(mods$mod[[4]])$dev.expl
+
+toplo <- mods |> 
+  select(bay_segment, prds) |> 
+  mutate(prds = purrr::map(prds, as_tibble)) |>
+  unnest(prds)
+
+ggplot(toplo, aes(x = date, y = chla)) +
+  geom_point() + 
+  geom_line(aes(y = btfit), color = 'red') + 
+  # coord_cartesian(xlim = c(2000, 2024)) +
+  # scale_y_log10() + 
+  facet_wrap(~ bay_segment, scales = 'free_y') +
+  theme_minimal()
+
+toplo <- mods |> 
+  select(bay_segment, prds) |> 
+  mutate(prds = purrr::map(prds, as_tibble)) |>
+  unnest(prds)
+
+ggplot(toplo, aes(x = log(chla), y = fit)) +
+  geom_point() +
+  geom_abline(slope = 1, intercept = 0, color = 'red') +
+  geom_smooth(method = 'lm', color = 'blue', se = F, formula = y ~ x) +
+  facet_wrap(~ bay_segment, scales = 'free') +
+  theme_minimal()
+
+# use toplo to get r2 between logchla and fit
+r2vals <- toplo |> 
+  mutate(
+    logchla = log(chla)
+  ) |> 
+  summarise(
+    r2 = cor(logchla, fit, use = 'complete.obs')^2,
+    .by = bay_segment
+  )
 
 toplo <- mods |>
   select(bay_segment, annsum) |> 
-  unnest(annsum) |> 
+  unnest(annsum) 
   
 ggplot(toplo, aes(x = yr, y = btfit)) +
   geom_point() + 
@@ -195,3 +257,6 @@ ggplot(toplo, aes(x = chla, y = btfit)) +
   geom_abline(slope = 1, intercept = 0, color = 'red') +
   facet_grid(bay_segment ~ mo, scales = 'free') +
   theme_minimal()
+
+# residuals and hydro load -----------------------------------------------
+
