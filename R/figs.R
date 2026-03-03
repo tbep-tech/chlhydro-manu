7190bb5ef7f4ad9b1718437bb89f6ffcb8eaf4eb
fawda123 <mbafs2012@gmail.com>
2026-03-03 18:06:47 -0500

    add better salvload plot, change season to quarter in text

--- R/figs.R
+++ R/figs.R
@@ -7,6 +7,7 @@ library(gratia)
 library(maps)
 library(ggspatial)
 library(sf)
+library(purrr)
 
 source(here('R/funcs.R'))
 
@@ -358,6 +359,16 @@ dev.off()
 
 # salinity v load effects ------------------------------------------------
 
+dat <- mods |>
+  select(bay_segment, data) |>
+  unnest(data) |>
+  mutate(yr = year(date)) |>
+  summarise(
+    tn_load = sum(tn_load, na.rm = TRUE),
+    sal = mean(sal, na.rm = TRUE),
+    .by = c(bay_segment, yr)
+  )
+
 toplo <- mods |>
   select(bay_segment, annsum) |>
   unnest(annsum) |>
@@ -366,54 +377,151 @@ toplo <- mods |>
     ldeff = btnorm - btnormmd
   ) |>
   select(bay_segment, yr, saleff, ldeff) |>
-  pivot_longer(
-    cols = c(saleff, ldeff),
-    names_to = 'var',
-    values_to = 'val'
-  )
+  left_join(dat, by = c('bay_segment', 'yr'))
 
-pa <- ggplot(toplo |> filter(var == 'saleff'), aes(x = yr, y = val)) +
+seg_order <- c('OTB', 'HB', 'MTB', 'LTB')
+
+# Primary axis: global range (total span across all segments)
+rng_tn <- diff(range(toplo$tn_load / 1000, na.rm = TRUE))
+rng_sal <- diff(range(toplo$sal, na.rm = TRUE))
+
+# Secondary axis: fixed ranges
+rng_ldeff <- diff(range(toplo$ldeff, na.rm = TRUE)) * 1.1 # actual range + 10% buffer
+rng_saleff <- 13 # spans -6.5 to 6.5
+
+# Single global slope per pair (same for all segments)
+slope_ld <- rng_tn / rng_ldeff
+slope_sal <- -rng_sal / rng_saleff # negative: inverse relationship
+
+# Fixed secondary axis breaks — identical across all panels
+ld_breaks <- pretty(c(-rng_ldeff / 2, rng_ldeff / 2))
+sal_breaks <- pretty(c(-rng_saleff / 2, rng_saleff / 2))
+
+dual_themesal <- theme(
+  legend.position = "none",
+  panel.grid = element_blank(),
+  panel.background = element_blank(),
+  axis.title.x = element_blank(),
+  axis.title.y.left = element_text(color = "black"),
+  # axis.text.y.left = element_text(color = "black"),
+  # axis.ticks.y.left = element_line(color = "black"),
+  axis.title.y.right = element_text(color = "cornflowerblue"),
+  axis.text.y.right = element_text(color = "cornflowerblue"),
+  axis.ticks.y.right = element_line(color = "cornflowerblue")
+)
+
+dual_themeld <- theme(
+  legend.position = "none",
+  panel.grid = element_blank(),
+  panel.background = element_blank(),
+  axis.title.x = element_blank(),
+  axis.title.y.left = element_text(color = "black"),
+  # axis.text.y.left = element_text(color = "black"),
+  # axis.ticks.y.left = element_line(color = "black"),
+  axis.title.y.right = element_text(color = "tomato1"),
+  axis.text.y.right = element_text(color = "tomato1"),
+  axis.ticks.y.right = element_line(color = "tomato1")
+)
+
+hide_left <- theme(axis.title.y.left = element_blank())
+hide_right <- theme(axis.title.y.right = element_blank())
+
+make_ld_plot <- function(seg) {
+  d <- filter(toplo, bay_segment == seg) |> mutate(tn_load = tn_load / 1000)
+  mid_tn <- mean(range(d$tn_load, na.rm = TRUE))
+  intcp <- mid_tn
+  d <- mutate(d, ldeff_scaled = slope_ld * ldeff + intcp)
+  ylim <- c(mid_tn - rng_tn / 2, mid_tn + rng_tn / 2)
+  p <- ggplot(d, aes(x = yr)) +
+    geom_line(aes(y = tn_load), color = "grey30") +
+    geom_line(aes(y = ldeff_scaled), color = "tomato1") +
+    scale_y_continuous(
+      name = "tons (x1000)",
+      limits = ylim,
+      sec.axis = sec_axis(
+        ~ (. - intcp) / slope_ld,
+        name = "Effect (\u03bcg/L)",
+        breaks = ld_breaks
+      )
+    ) +
+    dual_themeld
+  if (seg != 'OTB') {
+    p <- p + hide_left
+  }
+  if (seg != 'LTB') {
+    p <- p + hide_right
+  }
+  p
+}
+
+make_sal_plot <- function(seg) {
+  d <- filter(toplo, bay_segment == seg)
+  mid_sal <- mean(range(d$sal, na.rm = TRUE))
+  intcp <- mid_sal
+  d <- mutate(d, saleff_scaled = slope_sal * saleff + intcp)
+  ylim <- c(mid_sal - rng_sal / 2, mid_sal + rng_sal / 2)
+  p <- ggplot(d, aes(x = yr)) +
+    geom_line(aes(y = sal), color = "grey30") +
+    geom_line(aes(y = saleff_scaled), color = "dodgerblue3") +
+    scale_y_continuous(
+      name = "Salinity (ppt)",
+      limits = ylim,
+      trans = "reverse",
+      sec.axis = sec_axis(
+        ~ (. - intcp) / slope_sal,
+        name = "Effect (\u03bcg/L)",
+        breaks = sal_breaks
+      )
+    ) +
+    dual_themesal
+  if (seg != 'OTB') {
+    p <- p + hide_left
+  }
+  if (seg != 'LTB') {
+    p <- p + hide_right
+  }
+  p
+}
+
+toplo_eff <- toplo |>
+  mutate(bay_segment = factor(bay_segment, levels = seg_order)) |>
+  pivot_longer(cols = c(saleff, ldeff), names_to = 'var', values_to = 'val')
+
+pa <- ggplot(toplo_eff |> filter(var == 'saleff'), aes(x = yr, y = val)) +
   geom_hline(yintercept = 0, linetype = 'solid', color = 'black') +
   geom_point(color = 'dodgerblue3', size = 1) +
   facet_wrap(~bay_segment, ncol = 4) +
-  geom_smooth(
-    method = 'lm',
-    se = T,
-    color = 'dodgerblue3'
-  ) +
-  labs(
-    subtitle = '(a) Salinity Effect',
-    x = NULL,
-    y = 'µg/L (+ is lower salinity)',
-  ) +
+  geom_smooth(method = 'lm', se = TRUE, color = 'dodgerblue3') +
+  labs(subtitle = '(a) Salinity Effect', x = NULL, y = 'Effect (\u03bcg/L)') +
   theme_minimal() +
   theme(
-    axis.text.x = element_blank(),
-    panel.grid.minor = element_blank()
+    axis.ticks.y = element_line(),
+    panel.grid.minor = element_blank(),
+    panel.spacing.x = unit(1.5, 'cm')
   )
-pb <- ggplot(toplo |> filter(var == 'ldeff'), aes(x = yr, y = val)) +
+
+pb <- ggplot(toplo_eff |> filter(var == 'ldeff'), aes(x = yr, y = val)) +
   geom_hline(yintercept = 0, linetype = 'solid', color = 'black') +
   geom_point(color = 'tomato1', size = 1) +
   facet_wrap(~bay_segment, ncol = 4) +
-  geom_smooth(
-    method = 'lm',
-    se = T,
-    color = 'tomato1'
-  ) +
-  labs(
-    subtitle = '(b) Load Effect',
-    x = NULL,
-    y = 'µg/L (+ is higher load)',
-  ) +
+  geom_smooth(method = 'lm', se = TRUE, color = 'tomato1') +
+  labs(subtitle = '(b) Load Effect', x = NULL, y = 'Effect (\u03bcg/L)') +
   theme_minimal() +
   theme(
-    strip.text = element_blank(),
-    panel.grid.minor = element_blank()
+    axis.ticks.y = element_line(),
+    panel.grid.minor = element_blank(),
+    panel.spacing.x = unit(1.5, 'cm')
   )
 
-p <- pa + pb + plot_layout(ncol = 1, guides = 'collect')
+sal_plots <- purrr::map(seg_order, make_sal_plot)
+ld_plots <- purrr::map(seg_order, make_ld_plot)
 
-png(here('figs/salvload.png'), width = 7, height = 6, units = 'in', res = 300)
+p <- pa /
+  wrap_plots(sal_plots, nrow = 1) /
+  pb /
+  wrap_plots(ld_plots, nrow = 1)
+
+png(here('figs/salvload.png'), width = 9, height = 8, units = 'in', res = 300)
 print(p)
 dev.off()
 
--- R/scratch.R
+++ R/scratch.R
@@ -1,315 +1,161 @@
-library(tbeptools)
 library(tidyverse)
-library(mgcv)
-library(here)
-library(gratia)
-library(ggrepel)
 library(patchwork)
-library(flextable)
 
-source(here('R/funcs.R'))
+load(file = 'data/mods.RData')
 
-# data prep --------------------------------------------------------------
-
-load(file = here('data/wqdat.RData'))
-load(file = here('data/lddat.RData'))
-
-lddatlag <- lddat |>
+dat <- mods |>
+  select(bay_segment, data) |>
+  unnest(data) |>
+  mutate(
+    yr = year(date)
+  ) |>
   summarise(
-    tn_load = sum(tn_load),
-    .by = c(bay_segment, date)
-  ) |>
-  mutate(
-    lag1 = dplyr::lag(tn_load, n = 1),
-    lag2 = dplyr::lag(tn_load, n = 2),
-    lag3 = dplyr::lag(tn_load, n = 3),
-    .by = c(bay_segment)
-  ) |>
-  fill(
-    lag1,
-    lag2,
-    lag3,
-    .by = c(bay_segment),
-    .direction = 'up'
-  ) |>
-  mutate(
-    tn_load0 = tn_load,
-    tn_load1 = tn_load + lag1,
-    tn_load2 = tn_load + lag1 + lag2,
-    tn_load3 = tn_load + lag1 + lag2 + lag3
-  ) |>
-  select(-tn_load, -lag1, -lag2, -lag3)
-
-wqdat <- wqdat |>
-  select(-tn_load) |>
-  left_join(lddatlag, by = c('bay_segment', 'date'))
-
-mods <- wqdat |>
-  filter(dec_time >= 1985) |>
-  pivot_longer(
-    cols = starts_with('tn_load'),
-    names_to = 'tn_load_lag',
-    values_to = 'tn_load'
-  ) |>
-  group_nest(bay_segment, tn_load_lag) |>
-  mutate(
-    data = purrr::map(
-      data,
-      function(x) {
-        salmod <- gam(
-          sal ~ s(doy, bs = 'cc'),
-          data = x,
-          method = 'REML',
-          knots = list(doy = c(0, 366))
-        )
-        x$sal <- residuals(salmod)
-        tnmod <- gam(
-          tn_load ~ s(doy, bs = 'cc') + s(sal),
-          data = x,
-          method = 'REML',
-          knots = list(doy = c(0, 366))
-        )
-        x$tn_load <- residuals(tnmod)
-        x
-      }
-    ),
-    mod = purrr::map(
-      data,
-      ~ gam(
-        chla ~ s(dec_time, k = 40, bs = 'tp') +
-          s(doy, k = 10, bs = 'cc') +
-          s(sal, k = 10, bs = 'tp') +
-          s(tn_load, k = 10, bs = 'tp') +
-          ti(dec_time, doy, k = c(5, 5), bs = c('tp', 'cc')) +
-          ti(dec_time, sal, k = c(5, 5), bs = c('tp', 'tp')) +
-          ti(sal, doy, k = c(5, 5), bs = c('tp', 'cc')) +
-          ti(dec_time, tn_load, k = c(5, 5), bs = c('tp', 'tp')) +
-          ti(tn_load, doy, k = c(5, 5), bs = c('tp', 'cc')) +
-          ti(tn_load, sal, k = c(5, 5), bs = c('tp', 'tp')),
-        data = .x,
-        knots = list(doy = c(0, 366)),
-        family = Gamma(link = 'log'),
-        na.action = na.exclude,
-        method = 'REML'
-      )
-    ),
-    prds = purrr::map2(data, mod, pred_fun),
-    annsum = purrr::map(
-      prds,
-      ~ data.frame(.x) |>
-        mutate(
-          yr = lubridate::year(date)
-        ) |>
-        summarise(
-          chla = mean(chla, na.rm = T),
-          btfit = mean(btfit, na.rm = T),
-          btnorm = mean(btnorm, na.rm = T),
-          btfitmd = mean(btfitmd, na.rm = T),
-          btnormmd = mean(btnormmd, na.rm = T),
-          .by = c(yr)
-        )
-    )
+    tn_load = sum(tn_load, na.rm = T),
+    sal = mean(sal, na.rm = T),
+    .by = c(bay_segment, yr)
   )
-
-
-totab <- mods |>
-  select(bay_segment, tn_load_lag, mod) |>
-  mutate(
-    summ = purrr::map(mod, function(x) {
-      summmod <- summary(x)
-      GCV <- round(summmod$sp.criterion[[1]], 0)
-      devexpl <- round(summmod$dev.expl, 2)
-
-      out <- bind_cols(GCV = GCV, devexpl = devexpl) |>
-        rename(
-          `Dev. Expl.` = devexpl
-        )
-
-      return(out)
-    })
-  ) |>
-  select(-mod) |>
-  unnest('summ') |>
-  mutate(
-    bay_segment = ifelse(
-      duplicated(bay_segment),
-      '',
-      as.character(bay_segment)
-    ),
-    tn_load_lag = case_when(
-      tn_load_lag == 'tn_load0' ~ 'None',
-      tn_load_lag == 'tn_load1' ~ '1',
-      tn_load_lag == 'tn_load2' ~ '2',
-      tn_load_lag == 'tn_load3' ~ '3'
-    )
-  )
-
-suppgamtab <- totab |>
-  flextable() |>
-  set_header_labels(bay_segment = 'Bay Segment', tn_load_lag = 'TN Load Lag') |>
-  padding(padding = 0, part = 'all') |>
-  font(part = 'all', fontname = 'Times New Roman') |>
-  colformat_double(
-    j = 3,
-    big.mark = '',
-    digits = 0
-  ) |>
-  autofit() |>
-  hline(i = 4) |>
-  hline(i = 8) |>
-  hline(i = 12)
-
 toplo <- mods |>
-  filter(tn_load_lag == 'tn_load3') |>
-  select(bay_segment, annsum) |>
-  unnest(annsum)
-
-p <- ggplot(toplo, aes(x = yr, y = btfit)) +
-  geom_point(aes(fill = "Predicted"), color = 'black') +
-  geom_line(aes(y = btnorm, color = "Normalized", linetype = "Normalized")) +
-  geom_line(aes(
-    y = btnormmd,
-    color = "Normalized, Mean Load",
-    linetype = "Normalized, Mean Load"
-  )) +
-  scale_linetype_manual(
-    values = c("Normalized" = "solid", "Normalized, Mean Load" = "dashed")
-  ) +
-  scale_color_manual(
-    values = c(
-      "Normalized" = "tomato1",
-      "Normalized, Mean Load" = "dodgerblue3"
-    )
-  ) +
-  scale_fill_manual(values = c("Predicted" = "black")) +
-  facet_wrap(~bay_segment, scales = 'free_y') +
-  theme_minimal() +
-  theme(
-    legend.position = 'bottom',
-    panel.grid.minor = element_blank()
-  ) +
-  labs(
-    x = NULL,
-    color = NULL,
-    fill = NULL,
-    linetype = NULL,
-    y = 'Annual Chl-a (µg/L)',
-  )
-
-
-toplo1 <- mods |>
-  filter(tn_load_lag == 'tn_load3') |>
   select(bay_segment, annsum) |>
   unnest(annsum) |>
-  # filter(bay_segment == 'OTB') |>
-  select(bay_segment, yr, btfit, btnorm, btnormmd) |>
   mutate(
     saleff = btfit - btnorm,
-    ldeff = btnorm - btnormmd,
-    yrgrp = case_when(
-      yr < 2000 ~ '1985 - 1999',
-      yr >= 2000 & yr < 2015 ~ '2000 - 2014',
-      yr >= 2015 ~ '2015 - 2024'
-    )
-  )
+    ldeff = btnorm - btnormmd
+  ) |>
+  select(bay_segment, yr, saleff, ldeff) |>
+  left_join(dat, by = c('bay_segment', 'yr'))
 
-toplo2 <- toplo1 |>
-  summarise(
-    saleffmn = mean(saleff),
-    saleffhi = t.test(saleff)$conf.int[2],
-    salefflo = t.test(saleff)$conf.int[1],
-    ldeffmn = mean(ldeff),
-    ldeffhi = t.test(ldeff)$conf.int[2],
-    ldefflo = t.test(ldeff)$conf.int[1],
-    .by = c(yrgrp, bay_segment)
-  )
+seg_order <- c('OTB', 'HB', 'MTB', 'LTB')
 
-thm <- theme_minimal() +
+# Primary axis: global range (total span across all segments)
+rng_tn <- diff(range(toplo$tn_load / 1000, na.rm = TRUE))
+rng_sal <- diff(range(toplo$sal, na.rm = TRUE))
+
+# Secondary axis: fixed ranges
+rng_ldeff <- diff(range(toplo$ldeff, na.rm = TRUE)) * 1.1 # actual range + 10% buffer
+rng_saleff <- 13 # spans -6.5 to 6.5
+
+# Single global slope per pair (same for all segments)
+slope_ld <- rng_tn / rng_ldeff
+slope_sal <- -rng_sal / rng_saleff # negative: inverse relationship
+
+# Fixed secondary axis breaks — identical across all panels
+ld_breaks <- pretty(c(-rng_ldeff / 2, rng_ldeff / 2))
+sal_breaks <- pretty(c(-rng_saleff / 2, rng_saleff / 2))
+
+dual_theme <- theme(
+  legend.position = "none",
+  panel.grid = element_blank(),
+  panel.background = element_blank(),
+  axis.title.x = element_blank(),
+  axis.title.y.left = element_text(color = "tomato"),
+  axis.text.y.left = element_text(color = "tomato"),
+  axis.ticks.y.left = element_line(color = "tomato"),
+  axis.title.y.right = element_text(color = "cornflowerblue"),
+  axis.text.y.right = element_text(color = "cornflowerblue"),
+  axis.ticks.y.right = element_line(color = "cornflowerblue")
+)
+
+hide_left <- theme(axis.title.y.left = element_blank())
+hide_right <- theme(axis.title.y.right = element_blank())
+
+make_ld_plot <- function(seg) {
+  d <- filter(toplo, bay_segment == seg) |> mutate(tn_load = tn_load / 1000)
+  mid_tn <- mean(range(d$tn_load, na.rm = TRUE))
+  intcp <- mid_tn # effect = 0 maps to primary center
+  d <- mutate(d, ldeff_scaled = slope_ld * ldeff + intcp)
+  ylim <- c(mid_tn - rng_tn / 2, mid_tn + rng_tn / 2)
+  p <- ggplot(d, aes(x = yr)) +
+    geom_line(aes(y = tn_load), color = "tomato") +
+    geom_line(aes(y = ldeff_scaled), color = "cornflowerblue") +
+    scale_y_continuous(
+      name = "tons (x1000)",
+      limits = ylim,
+      sec.axis = sec_axis(
+        ~ (. - intcp) / slope_ld,
+        name = "Effect (\u03bcg/L)",
+        breaks = ld_breaks
+      )
+    ) +
+    dual_theme
+  if (seg != 'OTB') {
+    p <- p + hide_left
+  }
+  if (seg != 'LTB') {
+    p <- p + hide_right
+  }
+  p
+}
+
+make_sal_plot <- function(seg) {
+  d <- filter(toplo, bay_segment == seg)
+  mid_sal <- mean(range(d$sal, na.rm = TRUE))
+  intcp <- mid_sal # effect = 0 maps to primary center
+  d <- mutate(d, saleff_scaled = slope_sal * saleff + intcp)
+  ylim <- c(mid_sal - rng_sal / 2, mid_sal + rng_sal / 2)
+  p <- ggplot(d, aes(x = yr)) +
+    geom_line(aes(y = sal), color = "tomato") +
+    geom_line(aes(y = saleff_scaled), color = "cornflowerblue") +
+    scale_y_continuous(
+      name = "Salinity (ppt)",
+      limits = ylim,
+      trans = "reverse",
+      sec.axis = sec_axis(
+        ~ (. - intcp) / slope_sal,
+        name = "Effect (\u03bcg/L)",
+        breaks = sal_breaks
+      )
+    ) +
+    dual_theme
+  if (seg != 'OTB') {
+    p <- p + hide_left
+  }
+  if (seg != 'LTB') {
+    p <- p + hide_right
+  }
+  p
+}
+
+pA <- map(seg_order, make_ld_plot) |> wrap_plots()
+pA_sal <- map(seg_order, make_sal_plot) |> wrap_plots()
+
+# Combined 4x4 plot -----------------------------------------------------------
+
+# Rebuild pa and pb from figs.R with correct segment ordering
+toplo_eff <- toplo |>
+  mutate(bay_segment = factor(bay_segment, levels = seg_order)) |>
+  pivot_longer(cols = c(saleff, ldeff), names_to = 'var', values_to = 'val')
+
+pa <- ggplot(toplo_eff |> filter(var == 'saleff'), aes(x = yr, y = val)) +
+  geom_hline(yintercept = 0, linetype = 'solid', color = 'black') +
+  geom_point(color = 'dodgerblue3', size = 1) +
+  facet_wrap(~bay_segment, ncol = 4) +
+  geom_smooth(method = 'lm', se = TRUE, color = 'dodgerblue3') +
+  labs(subtitle = '(a) Salinity Effect', x = NULL, y = 'Effect (\u03bcg/L)') +
+  theme_minimal() +
   theme(
-    panel.grid.minor = element_blank()
+    axis.ticks.y = element_line(),
+    panel.grid.minor = element_blank(),
+    panel.spacing.x = unit(1.2, "cm")
   )
 
-# create path by year
-set.seed(123)
-p1 <- ggplot(toplo1, aes(x = saleff, y = ldeff)) +
-  geom_hline(yintercept = 0, linetype = "dashed", color = "black") +
-  geom_vline(xintercept = 0, linetype = "dashed", color = "black") +
-  geom_path(aes(group = 1), color = 'gray80', linewidth = 1) +
-  geom_point(aes(color = yrgrp), size = 2) +
-  geom_text_repel(
-    aes(label = yr, color = yrgrp),
-    size = 2,
-    max.overlaps = 20
-  ) +
-  scale_color_viridis_d(option = "D", end = 0.8) +
-  # scale_fill_viridis_d(option = "D", end = 0.8) +
-  # coord_equal() +
-  scale_y_continuous(expand = c(0.1, 0.1)) +
-  scale_x_continuous(expand = c(0.1, 0.1)) +
-  facet_wrap(~bay_segment, scales = 'free') +
-  labs(
-    color = "Year",
-    x = 'Salinity Effect µg/L (+ is lower salinity)',
-    y = 'Load Effect µg/L (+ is higher load)',
-    subtitle = '(a) By Year'
+pb <- ggplot(toplo_eff |> filter(var == 'ldeff'), aes(x = yr, y = val)) +
+  geom_hline(yintercept = 0, linetype = 'solid', color = 'black') +
+  geom_point(color = 'tomato1', size = 1) +
+  facet_wrap(~bay_segment, ncol = 4) +
+  geom_smooth(method = 'lm', se = TRUE, color = 'tomato1') +
+  labs(subtitle = '(b) Load Effect', x = NULL, y = 'Effect (\u03bcg/L)') +
+  theme_minimal() +
+  theme(
+    axis.ticks.y = element_line(),
+    panel.grid.minor = element_blank(),
+    panel.spacing.x = unit(1.2, "cm")
   )
 
-# create yrgrp means
-set.seed(123)
-p2 <- ggplot(toplo1, aes(x = saleff, y = ldeff)) +
-  geom_hline(yintercept = 0, linetype = "dashed", color = "black") +
-  geom_vline(xintercept = 0, linetype = "dashed", color = "black") +
-  geom_point(alpha = 0) +
-  geom_point(
-    data = toplo2,
-    aes(x = saleffmn, y = ldeffmn, color = yrgrp),
-    size = 4,
-    show.legend = F
-  ) +
-  geom_errorbar(
-    data = toplo2,
-    aes(
-      x = saleffmn,
-      y = ldeffmn,
-      xmin = salefflo,
-      xmax = saleffhi,
-      color = yrgrp
-    ),
-    orientation = 'y',
-    show.legend = F,
-    lineend = 'square',
-    width = 0
-  ) +
-  geom_errorbar(
-    data = toplo2,
-    aes(
-      x = saleffmn,
-      y = ldeffmn,
-      ymin = ldefflo,
-      ymax = ldeffhi,
-      color = yrgrp
-    ),
-    show.legend = F,
-    lineend = 'square',
-    width = 0
-  ) +
-  geom_text_repel(
-    aes(label = yr),
-    alpha = 0,
-    max.overlaps = 20
-  ) +
-  scale_color_viridis_d(option = "D", end = 0.8) +
-  scale_y_continuous(expand = c(0.1, 0.1)) +
-  scale_x_continuous(expand = c(0.1, 0.1)) +
-  facet_wrap(~bay_segment, scales = 'free') +
-  labs(
-    color = "Year",
-    x = 'Salinity Effect µg/L (+ is lower salinity)',
-    y = 'Load Effect µg/L (+ is higher load)',
-    subtitle = '(b) Means (+/- 95% Confidence) by Year Group'
-  )
+# Individual plots in segment order
+sal_plots <- purrr::map(seg_order, make_sal_plot)
+ld_plots <- purrr::map(seg_order, make_ld_plot)
 
-p <- p1 +
-  p2 +
-  plot_layout(ncol = 1, guides = 'collect', axis_titles = 'collect') &
-  thm
+pcombined <- pa /
+  wrap_plots(sal_plots, nrow = 1) /
+  pb /
+  wrap_plots(ld_plots, nrow = 1)
