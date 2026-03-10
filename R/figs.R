aa906df4a5450f62bc0bad88c982c4efe2ed04b9
fawda123 <mbafs2012@gmail.com>
2026-03-10 14:44:22 -0400

    add simload fig

--- R/figs.R
+++ R/figs.R
@@ -765,6 +765,111 @@ png(here('figs/simplo.png'), width = 8, height = 8, units = 'in', res = 300)
 print(p)
 dev.off()
 
+# simloads ---------------------------------------------------------------
+
+load(file = 'data/simprddat1524.RData')
+
+toplo <- simprddat1524 |>
+  filter(bay_segment == 'OTB') |>
+  select(bay_segment, exceedssum) |>
+  unnest('exceedssum') |>
+  filter(ldfac %in% c('TMDL Load Factor: 0.5', 'TMDL Load Factor: 1')) |>
+  mutate(
+    ldfac = factor(
+      ldfac,
+      levels = c('TMDL Load Factor: 0.5', 'TMDL Load Factor: 1')
+    )
+  )
+
+cols <- c(
+  'TMDL Load Factor: 0.5' = '#FC4E2A',
+  'TMDL Load Factor: 1' = '#800026'
+)
+
+# Year-50 endpoints for horizontal arrow/label annotations
+endpoints <- toplo |>
+  filter(yrs == 50) |>
+  select(ldfac, avexceeds)
+
+# Vertical segment showing reduction between the two scenarios at the y-axis
+vert_seg <- tibble(
+  y = endpoints$avexceeds[endpoints$ldfac == 'TMDL Load Factor: 1'],
+  yend = endpoints$avexceeds[endpoints$ldfac == 'TMDL Load Factor: 0.5'],
+) |>
+  mutate(
+    ymid = (y + yend) / 2,
+    label = paste0('-', round(y - yend, 1), '%')
+  )
+
+p <- ggplot(
+  toplo,
+  aes(x = yrs, y = avexceeds, color = ldfac, fill = ldfac)
+) +
+  geom_ribbon(
+    aes(ymin = avexceeds - sdexceeds, ymax = avexceeds + sdexceeds),
+    alpha = 0.2,
+    color = NA
+  ) +
+  geom_line(linewidth = 0.8) +
+  geom_segment(
+    data = endpoints,
+    aes(x = 50, xend = 0, y = avexceeds, yend = avexceeds),
+    linetype = 'dashed',
+    linewidth = 0.8,
+    arrow = arrow(length = unit(0.25, 'cm'), type = 'closed'),
+    show.legend = FALSE
+  ) +
+  geom_text(
+    data = endpoints,
+    aes(x = 0, y = avexceeds, label = paste0(round(avexceeds, 1), '%')),
+    hjust = -0.3,
+    vjust = -0.5,
+    size = 3.5,
+    show.legend = FALSE
+  ) +
+  geom_segment(
+    data = vert_seg,
+    aes(x = 0, xend = 0, y = y, yend = yend),
+    color = 'black',
+    linetype = 'dashed',
+    linewidth = 0.8,
+    arrow = arrow(length = unit(0.25, 'cm'), type = 'closed'),
+    inherit.aes = FALSE
+  ) +
+  geom_text(
+    data = vert_seg,
+    aes(x = 0, y = ymid, label = label),
+    hjust = -0.2,
+    size = 3.5,
+    color = 'black',
+    inherit.aes = FALSE
+  ) +
+  scale_x_continuous(limits = c(0, 50), breaks = seq(0, 50, by = 10)) +
+  # coord_cartesian(ylim = c(0, NA)) +
+  scale_color_manual(values = cols) +
+  scale_fill_manual(values = cols) +
+  theme_minimal(base_size = 9) +
+  theme(
+    panel.grid.minor = element_blank(),
+    legend.position = 'bottom'
+  ) +
+  labs(
+    x = 'Years from Simulation Period',
+    y = 'Likelihood of Exceeding Threshold',
+    color = NULL,
+    fill = NULL
+  )
+
+png(
+  here('figs/simload.png'),
+  width = 4.5,
+  height = 4.5,
+  units = 'in',
+  res = 300
+)
+print(p)
+dev.off()
+
 # # grid plot --------------------------------------------------------------
 
 # thresh <- tbeptools::targets |>
